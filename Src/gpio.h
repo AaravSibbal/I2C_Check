0876cb9f2992acc440036f3e3f9655ea85afd8ee
AaravSibbal <106462208+AaravSibbal@users.noreply.github.com>
2026-03-27 01:35:16 -0400

    gpio driver
    structed the gpio driver made an api that would make calling the gpio driver easier

--- Src/gpio.h
+++ Src/gpio.h
@@ -4,51 +4,99 @@
 #include <stdint.h>
 
 #define __IO volatile
+#define GPIOB_BASE  ((uint32_t)0x40020400)
+#define GPIOD_BASE ((uint32_t)0x40020C00)
 
-#define GPIOB_BASE (0x40020400)
-#define GPIOD_BASE (0x40020C00)
-
-#define GPIO_MODER_OFFSET (0x00)
-#define GPIO_OTYPER_OFFSET (0x04)
-#define GPIO_AFRL_OFFSET (0x20)
-#define GPIO_AFRH_OFFSET (0X24)
-#define GPIO_ODR_OFFSET (0X14)
-
-
-typedef enum{
+typedef enum GPIO_MODER {
     GPIO_MODE_INPUT = 0x00,
     GPIO_MODE_OUTPUT = 0x01,
     GPIO_MODE_ALT = 0x02,
     GPIO_MODE_ANALOG = 0x03
-} GPIO_Mode_t;
+} GPIO_MODER_t;
 
-typedef enum{
-    GPIO_PUSH_PULL = 0x00,
-    GPIO_OPEN_DRAIN = 0X01
+typedef enum GPIO_OTYPER{
+    GPIO_TYPE_PUSH_PULL = 0x00,
+    GPIO_TYPE_OPEN_DRAIN = 0X01
 } GPIO_OTYPER_t;
 
-typedef enum{
-    GPIO_OUTPUT_HIGH = 0x00,
-    GPIO_OUTPUT_LOW = 0x01
+typedef enum GPIO_ODR {
+    GPIO_OUTPUT_LOW = 0,
+    GPIO_OUTPUT_HIGH
 } GPIO_ODR_t;
 
+typedef enum GPIO_AFx {
+    AF0 = 0,
+    AF1,
+    AF2,
+    AF3,
+    AF4,
+    AF5,
+    AF6,
+    AF7,
+    AF8,
+    AF9,
+    AF10,
+    AF11,
+    AF12,
+    AF13,
+    AF14,
+    AF15
+}GPIO_AFx_t;
 
+typedef enum GPIO_BSSR_bit {
+    BSSR_RESET = 0,
+    BSSR_SET = 1
+}GPIO_BSSR_bit_t;
 
+typedef enum GPIO_BSSR_action {
+    BSSR_NO_ACTION = 0,
+    BSSR_TAKE_ACTION
+}GPIO_BSSR_action_t;
 
+typedef struct BSSR{
+    BSSR_bit_t bit_type;
+    BSSR_action_t action;
+}GPIO_BSSR_t;
 
+typedef enum GPIO_Pin {
+    GPIO_PIN_0 = 0,
+    GPIO_PIN_1,
+    GPIO_PIN_2,
+    GPIO_PIN_3,
+    GPIO_PIN_4,
+    GPIO_PIN_5,
+    GPIO_PIN_6,
+    GPIO_PIN_7,
+    GPIO_PIN_8,
+    GPIO_PIN_9,
+    GPIO_PIN_10,
+    GPIO_PIN_11,
+    GPIO_PIN_12,
+    GPIO_PIN_13,
+    GPIO_PIN_14,
+    GPIO_PIN_15
+
+}GPIO_Pin_t;
 
 typedef struct GPIO{
     __IO uint32_t MODER;
     __IO uint32_t OTYPER;
-    __IO uint32_t AFRL;
-    __IO uint32_t AFRH ;
-    __IO uint32_t ODR
+    __IO uint32_t GPIO_OSPEEDR;
+    __IO uint32_t GPIO_PUPDR;
+    __IO uint32_t GPIO_IDR;
+    __IO uint32_t ODR;
+    __IO uint32_t BSSR;
+    __IO uint32_t GPIO_LCKR;
+    __IO uint32_t AFR[2];
 } GPIO_t;
 
+#define GPIOB ((GPIO_t *) GPIOB_BASE)
+#define GPIOD ((GPIO_t *) GPIOD_BASE)
 
+void GPIO_set_moder(GPIO_t* gpio, GPIO_Pin_t pin, GPIO_MODER_t mode);
+void GPIO_set_otyper(GPIO_t* gpio, GPIO_Pin_t pin, GPIO_OTYPER_t type);
+void GPIO_set_odr(GPIO_t* gpio, GPIO_Pin_t pin, GPIO_ODR_t output);
+void GPIO_set_alt_func(GPIO_t* gpio, GPIO_Pin_t pin, GPIO_AFx_t function);
 
-void GPIO_set_moder()
-
-GPIO_t
 
 #endif
--- Src/main.c
+++ Src/main.c
@@ -1,21 +1,5 @@
 #include <stdint.h>
-
-// GPIOB 
-#define GPIOB_BASE (0x40020400)
-// alternate function 10 or 2
-#define GPIOB_MODER (*(volatile uint32_t *) (GPIOB_BASE + 0X00))
-#define GPIOB_OTYPER (*(volatile uint32_t *) (GPIOB_BASE + 0x04)) 
-#define GPIOB_AFRL (*(volatile uint32_t *) (GPIOB_BASE + 0x20)) // scl pin 6
-#define GPIOB_AFRH (*(volatile uint32_t *) (GPIOB_BASE + 0x24)) // sda pin 9
-
-// red and green lights both are on port D
-// green is on PD12 red is on PD 14
-#define GPIOD_BASE (0x40020C00)
-// this would be 1 as both red and green light would need to be in
-// output mode
-#define GPIOD_MODER (*(volatile uint32_t *) (GPIOD_BASE + 0X00))
-// this will control the output of the lights green and red
-#define GPIOD_ODR (*(volatile uint32_t *) (GPIOD_BASE + 0x14))
+#include "gpio.h"
 
 // reset and clock control (RCC) Clock control register
 #define RCC_BASE (0x40023800)
@@ -27,11 +11,9 @@
 // RCC ABP1 Enable register
 // i2c1 is bit 21
 #define RCC_ABP1ENR (*(volatile uint32_t *) (RCC_BASE + 0x40))
-
 /**
  * I2C definitions
  */
-
  #define I2C_BASE (0x40005400)
 //  I2C Clock control register
  #define I2C_CCR (*(volatile uint32_t *) (I2C_BASE + 0x1C))
@@ -59,16 +41,18 @@
 
 
 void turn_green_on(){
-    GPIOD_ODR |= (1<<12);
+    GPIO_set_odr(GPIOD, GPIO_PIN_12, GPIO_OUTPUT_HIGH);
 }
 void turn_red_on(){
-    GPIOD_ODR |= (1<<14);
+    GPIO_set_odr(GPIOD, GPIO_PIN_14, GPIO_OUTPUT_HIGH);
 }
 void turn_green_off(){
     GPIOD_ODR &= ~(1<<12);
+    GPIO_set_odr(GPIOD, GPIO_PIN_12, GPIO_OUTPUT_LOW);
 }
 void turn_red_off(){
     GPIOD_ODR &= ~(1<<14);
+    GPIO_set_odr(GPIOD, GPIO_PIN_14, GPIO_OUTPUT_LOW);
 }
 
 void GPIOx_clock_on(){
@@ -86,33 +70,23 @@ void init_clock_on(){
 }
 
 void init_GPIOD(){
-    // lights infrastructure
-
-    // clear the pin 12 section, i.e. green
-    GPIOD_MODER &= ~(3<<24);
-    GPIOD_MODER |= (1<<24);
-
-    GPIOD_MODER &= ~(3<<28);
-    GPIOD_MODER |= (1<<28);
+    GPIO_set_moder(GPIOD, GPIO_PIN_12, GPIO_MODE_OUTPUT);
+    GPIO_set_moder(GPIOD, GPIO_PIN_14, GPIO_MODE_OUTPUT);
 
 }
 
 // pin6
 void init_SCL(){
-    GPIOB_MODER &= ~(3<<12);
-    GPIOB_MODER |= (2<<12);
-    GPIOB_OTYPER |= (1<<6);
-    GPIOB_AFRL &= ~(15<<24);
-    GPIOB_AFRL |= (4<<24);    
+    GPIO_set_moder(GPIOB, GPIO_PIN_6, GPIO_MODE_ALT);
+    GPIO_set_otyper(GPIOB, GPIO_PIN_6, GPIO_TYPE_OPEN_DRAIN);
+    GPIO_set_alt_func(GPIOB, GPIO_PIN_6, AF4);
 }
 
 // pin 9 
 void init_SDA(){
-    GPIOB_MODER &= ~(3<<18);
-    GPIOB_MODER |= (2<<18);
-    GPIOB_OTYPER |= (1<<9);
-    GPIOB_AFRH &= ~(15<<4);
-    GPIOB_AFRH |= (4<<4);    
+    GPIO_set_moder(GPIOB, GPIO_PIN_9, GPIO_MODE_ALT);
+    GPIO_set_otyper(GPIOB, GPIO_PIN_9, GPIO_TYPE_OPEN_DRAIN);
+    GPIO_set_alt_func(GPIOB, GPIO_PIN_9, AF4);
 }
 
 void init_GPIOB(){
